6903aa822abfd2c7db12665b12731f169d3e8b13
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2018-10-28 22:05:25 +0200

    serialports format

--- src/plugins/serialports/serialports.ts
+++ src/plugins/serialports/serialports.ts
@@ -1,43 +1,39 @@
 import * as SerialPort from "serialport";
-import * as events from "events"
+import * as events from "events";
 import { knownDeviceList } from "./knownDeviceList";
 import { callbackify } from "util";
-import { Plugin } from "../plugin"
+import { Plugin } from "../plugin";
 
-var _ = require("lodash")
+var _ = require("lodash");
 
 // var usbDetect = require('usb-detection');
 // usbDetect.startMonitoring();
 
 export const name = "serialports";
 
-
-
 export const workflowDefinitions = [
   "var serialports = { ",
   "write: (comName:string, data:string, cb:Function)=>{}",
   "list: ()",
   "}"
-]
+];
 
 export const workflow = {
-  write : writeToPort,
-  list : listPorts
-}
+  write: writeToPort,
+  list: listPorts
+};
 
-export function writeToPort(comName:string, data:string, cb:Function) {
-  sp.write(comName,data, cb);
+export function writeToPort(comName: string, data: string, cb: Function) {
+  sp.write(comName, data, cb);
 }
 
 export function listPorts() {
   return sp.listOfPluggedInDevices;
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 export class SerialPorts extends events.EventEmitter {
   public listOfPluggedInDevices: any = [];
@@ -52,7 +48,6 @@ export class SerialPorts extends events.EventEmitter {
     this.list((allports: any) => {
       this.listOfPluggedInDevices = [];
       for (var port in allports) {
-
         if (allports[port].pnpId) {
           this.listOfPluggedInDevices.push(allports[port]);
         }
@@ -66,14 +61,13 @@ export class SerialPorts extends events.EventEmitter {
             this.listOfPluggedInDevices.push(allports[port]);
           }
         }*/
-
       }
       cb();
-    })
+    });
   }
 
   async list(cb: any) {
-    var serialportList = await SerialPort.list()
+    var serialportList = await SerialPort.list();
     cb(serialportList);
   }
 
@@ -84,32 +78,36 @@ export class SerialPorts extends events.EventEmitter {
       var alreadyPlugged = false;
 
       for (var port in this.listofConnectedPorts) {
-        if (this.listOfPluggedInDevices[device].comName == this.listofConnectedPorts[port].device.comName) {
+        if (
+          this.listOfPluggedInDevices[device].comName ==
+          this.listofConnectedPorts[port].device.comName
+        ) {
           alreadyPlugged = true;
         }
       }
 
-
       if (alreadyPlugged === false) {
         //var dev = new SerialJSONDevice(this.listOfPluggedInDevices[device].comName, { baudRate: this.listOfPluggedInDevices[device].baudRate})
 
+        var dev = new SerialJSONDevice(this.listOfPluggedInDevices[device], {
+          baudRate: 115200
+        });
 
-        var dev = new SerialJSONDevice(this.listOfPluggedInDevices[device], { baudRate: 115200 })
-
-        dev.on("connect", (device)=> {
+        dev.on("connect", device => {
           this.emit("connect", device);
-        })
-
+        });
 
         this.listofConnectedPorts.push(dev);
-        dev.on("json", (json) => {
-          
-          //console.log(json) 
+        dev.on("json", json => {
+          //console.log(json)
           this.emit("data", json);
-        })
-        dev.on("close", (err) => {
-
-          this.listofConnectedPorts = this.listofConnectedPorts.filter((device: any) => { device.comName == dev.device.comName })
+        });
+        dev.on("close", err => {
+          this.listofConnectedPorts = this.listofConnectedPorts.filter(
+            (device: any) => {
+              device.comName == dev.device.comName;
+            }
+          );
 
           // for (var port in this.listofConnectedPorts) {
           //   if (this.listofConnectedPorts[port].device.comName == dev.device.comName) {
@@ -117,11 +115,9 @@ export class SerialPorts extends events.EventEmitter {
           //   }
           // }
 
-
-          this.emit("close", { err: err, device: dev.device })
-        })
+          this.emit("close", { err: err, device: dev.device });
+        });
       }
-
     }
   }
   /* end of scan() */
@@ -131,10 +127,10 @@ export class SerialPorts extends events.EventEmitter {
     }
   }
 
-  write(comName:string, data:string, cb:Function) {
+  write(comName: string, data: string, cb: Function) {
     for (let device of this.listofConnectedPorts) {
       if (device.device.comName == comName) {
-        device.write(data);        
+        device.write(data);
         cb(undefined, "success");
         return;
       }
@@ -142,23 +138,19 @@ export class SerialPorts extends events.EventEmitter {
 
     cb("not found", undefined);
   }
-
-
 }
-
 
 declare namespace SerialPortCustom {
-	interface PortConfig {
-		comName: string;
-		manufacturer: string;
-		serialNumber: string;
-		pnpId: string;
-		locationId: string;
-		vendorId: string;
-		productId: string;
+  interface PortConfig {
+    comName: string;
+    manufacturer: string;
+    serialNumber: string;
+    pnpId: string;
+    locationId: string;
+    vendorId: string;
+    productId: string;
   }
 }
-
 
 export class SerialJSONDevice extends events.EventEmitter {
   mybuffer: string = "";
@@ -170,37 +162,34 @@ export class SerialJSONDevice extends events.EventEmitter {
 
     this.device = device;
 
-    this.port = new SerialPort(device.comName, options, (err) => {
-      if (err) { console.error(err) } else {
-        this.emit("connect", device)
+    this.port = new SerialPort(device.comName, options, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        this.emit("connect", device);
       }
-    })
+    });
 
     this.port.on("data", (data: any) => {
       this.mybuffer += data.toString();
 
-
       if (this.mybuffer.indexOf("\r") > 0) {
         var splitbuffer = this.mybuffer.split("\r");
-        var cleanoutput = splitbuffer[0].split("\n").join("")
-        this.emit("serial", cleanoutput)
-
+        var cleanoutput = splitbuffer[0].split("\n").join("");
+        this.emit("serial", cleanoutput);
 
         var err;
         var parsedJson;
-        try { 
+        try {
           parsedJson = JSON.parse(cleanoutput);
-        } catch(err) {
-
-        }
+        } catch (err) {}
         var jsonData = {
           device: this.device,
           options: options,
           raw: cleanoutput,
           datajson: parsedJson,
-          err :err
-        }
-
+          err: err
+        };
 
         /*
         comName:{ serialport.comName}<br /> 
@@ -211,24 +200,20 @@ export class SerialJSONDevice extends events.EventEmitter {
         */
 
         //console.log(jsonData);
-        this.emit('json', jsonData);
-
+        this.emit("json", jsonData);
 
         this.mybuffer = splitbuffer[1];
       }
-    })
+    });
 
     this.port.on("close", (err: Error) => {
       this.emit("close", err);
-    })
+    });
 
     this.port.on("open", () => {
-      console.log("event open")
-    })
-
+      console.log("event open");
+    });
   }
-
-
 
   write(data: any) {
     //console.log("writing to port")
@@ -236,77 +221,84 @@ export class SerialJSONDevice extends events.EventEmitter {
     this.port.write(data + "\n");
     //return "Hello, " + this.comName;
   }
-
-
 }
 
-
-var sp:SerialPorts;
+var sp: SerialPorts;
 
 export function init(app: any, db: any, eventHub: events.EventEmitter) {
-
-  console.log("initializing serialports")
+  console.log("initializing serialports");
 
   sp = new SerialPorts();
 
-  sp.on('connect', (device) => {
+  sp.on("connect", device => {
     console.log("--- connect ---");
-    updateserialportdevice(db, device, (err:Error, res:any)=>{
-
-    })
+    updateserialportdevice(db, device, (err: Error, res: any) => {});
     //console.log(device);
     //eventHub.emit("device",{apikey:"cp3rrhjeryufe2xnlsxk7yooxat2m3rj", packet: {id:"testSerial", data:{a:1}}    })
-  })
+  });
 
-  sp.on('data', (data) => {
-
+  sp.on("data", data => {
     var packet = {
       id: data.device.pnpId,
       data: data,
       meta: { method: "serialport" }
-    }
+    };
 
     packet.data.connected = true;
-    packet.data.err = ""
+    packet.data.err = "";
     //delete packet.data["serialNumber"]
     //delete packet.data["locationId"]
 
-    getserialportconfig(db, data.device, (err:any, config:any)=>{
-      if (config == null) { return; }
+    getserialportconfig(db, data.device, (err: any, config: any) => {
+      if (config == null) {
+        return;
+      }
       if (config.apikey) {
-        
-        eventHub.emit("device", { apikey: config.apikey, packet: packet })
+        eventHub.emit("device", { apikey: config.apikey, packet: packet });
 
         //////
         try {
           if (data.datajson.id) {
-            
-            eventHub.emit("device", { apikey: config.apikey, packet: { id : data.datajson.id, level:1, data: data.datajson.data, serial: data , meta: { method: "serialport" } } } )
+            eventHub.emit("device", {
+              apikey: config.apikey,
+              packet: {
+                id: data.datajson.id,
+                level: 1,
+                data: data.datajson.data,
+                serial: data,
+                meta: { method: "serialport" }
+              }
+            });
 
             try {
-                var rx = JSON.parse(data.datajson.rx.buf)
+              var rx = JSON.parse(data.datajson.rx.buf);
 
-                if (rx.id) {
-                  eventHub.emit("device", { apikey: config.apikey, packet: { id : rx.id, level: 2, data: rx.data, serial: data, meta: { method: "serialportRxTx" } } } )
-                }
-                
-            }catch(err) {
-
-            }
-
+              if (rx.id) {
+                eventHub.emit("device", {
+                  apikey: config.apikey,
+                  packet: {
+                    id: rx.id,
+                    level: 2,
+                    data: rx.data,
+                    serial: data,
+                    meta: { method: "serialportRxTx" }
+                  }
+                });
+              }
+            } catch (err) {}
           }
         } catch (err) {}
         //////
       }
-    })
+    });
+  });
 
-    
-  })
+  sp.on("close", data => {
+    var error = data.err.toString();
 
-  sp.on("close", (data) => {
-    var error = data.err.toString()
-
-    if (error == "Error: bad file descriptor") { error = "Unplugged" }
+    if (error == "Error: bad file descriptor") {
+      error = "Unplugged";
+    }
 
     var packet = {
       id: data.device.pnpId,
@@ -315,71 +307,82 @@ export function init(app: any, db: any, eventHub: events.EventEmitter) {
         err: error
       },
       meta: { method: "serialport" }
-    }
+    };
 
-    getserialportconfig(db, data.device, (err:any, config:any)=>{
-      if (config == null) { return; }
-      if (config.apikey) {
-        eventHub.emit("device", { apikey: config.apikey, packet: packet })
+    getserialportconfig(db, data.device, (err: any, config: any) => {
+      if (config == null) {
+        return;
       }
-    })
-
-  })
+      if (config.apikey) {
+        eventHub.emit("device", { apikey: config.apikey, packet: packet });
+      }
+    });
+  });
 
   setInterval(() => {
     sp.scan(() => {
       //console.log(sp.listOfPluggedInDevices);
       sp.connectDevices();
     });
-  }, 2500)
+  }, 2500);
 
   sp.scan(() => {
     //console.log(sp.listOfPluggedInDevices);
     sp.connectDevices();
   });
 
-
-  app.get('/api/v3/serialports', (req: any, res: any) => {
-    db.plugins_serialports.find({}, (err:any, devicelist:any)=>{
+  app.get("/api/v3/serialports", (req: any, res: any) => {
+    db.plugins_serialports.find({}, (err: any, devicelist: any) => {
       res.json(devicelist);
-    })
+    });
     //res.json(sp.listOfPluggedInDevices)
-  })
+  });
 
-  app.post("/api/v3/serialports/setserialportaccount", (req:any, res:any)=>{
-    setserialportaccount(db, req.body, req.user, (err:Error, result:any)=>{
+  app.post("/api/v3/serialports/setserialportaccount", (req: any, res: any) => {
+    setserialportaccount(db, req.body, req.user, (err: Error, result: any) => {
       if (err) res.json(err);
       res.json(result);
-    })
-  })
+    });
+  });
 }
 
-
-function setserialportaccount(db:any, device:any, user:any, cb:any) {
-  db.plugins_serialports.update({pnpId:device.pnpId}, 
-    {"$set":{ "apikey" : user.apikey}}, (err:Error, resultUpd:any)=>{
-    if (err) cb(err,undefined);
-    if (resultUpd) { cb(undefined, resultUpd); }
-  })
-}
-
-
-function updateserialportdevice(db:any, device:any, cb:any) {
-  db.plugins_serialports.findOne({pnpId:device.pnpId}, (err:any, devicedb:any)=>{
-    var newdevice:any = {}
-    
-    if (devicedb == null) {      newdevice = _.clone(device);
-    } else {      newdevice = _.merge(devicedb, device)
+function setserialportaccount(db: any, device: any, user: any, cb: any) {
+  db.plugins_serialports.update(
+    { pnpId: device.pnpId },
+    { $set: { apikey: user.apikey } },
+    (err: Error, resultUpd: any) => {
+      if (err) cb(err, undefined);
+      if (resultUpd) {
+        cb(undefined, resultUpd);
+      }
     }
-
-    db.plugins_serialports.update({pnpId:newdevice.pnpId}, newdevice, {upsert:true}, (err:any, res:any)=>{
-      //console.log(res);
-    })
-
-  }) 
+  );
 }
 
+function updateserialportdevice(db: any, device: any, cb: any) {
+  db.plugins_serialports.findOne(
+    { pnpId: device.pnpId },
+    (err: any, devicedb: any) => {
+      var newdevice: any = {};
 
-function getserialportconfig(db:any, device:any, cb:any) {
-  db.plugins_serialports.findOne({pnpId:device.pnpId}, cb);
+      if (devicedb == null) {
+        newdevice = _.clone(device);
+      } else {
+        newdevice = _.merge(devicedb, device);
+      }
+
+      db.plugins_serialports.update(
+        { pnpId: newdevice.pnpId },
+        newdevice,
+        { upsert: true },
+        (err: any, res: any) => {
+          //console.log(res);
+        }
+      );
+    }
+  );
+}
+
+function getserialportconfig(db: any, device: any, cb: any) {
+  db.plugins_serialports.findOne({ pnpId: device.pnpId }, cb);
 }
