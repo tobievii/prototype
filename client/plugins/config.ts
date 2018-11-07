8f125e3b6f443428a7f6fda19dfc1d4bd7336da3
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2018-11-07 11:13:14 +0200

    default config

--- client/plugins/config.ts
+++ client/plugins/config.ts
@@ -1,7 +1,7 @@
 export var plugins = []
 
 plugins.push(require("./account/pluginAccount.jsx"))
-//plugins.push(require("./iotnxt/pluginIotnxt.jsx"))
+plugins.push(require("./iotnxt/pluginIotnxt.jsx"))
 plugins.push(require("./discord/discord.jsx"))
 plugins.push(require("./http/pluginHTTP.jsx"))
 plugins.push(require("./tcp/pluginTcp.jsx"))
--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -1,14 +1,14 @@
 export var plugins:any = []
 
 // import * as serialports from "./serialports/serialports"
-// import * as iotnxt from "./iotnxt/iotnxtserverside"
+import * as iotnxt from "./iotnxt/iotnxtserverside"
 import * as tcpPlugin from "./tcp/pluginTcp_serverside"
 import * as discord from "./discord/discord"
 import * as mqttPlugin from "./mqttserver/mqttPlugin"
 import * as httpPlugin from "./http/pluginHTTP_serverside"
 
 // plugins.push(serialports)
-// plugins.push(iotnxt)
+plugins.push(iotnxt)
 plugins.push(tcpPlugin)
 plugins.push(discord);
 plugins.push(mqttPlugin);
