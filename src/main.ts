ab6c90baf7a966019d7d2d3004e76847f4b5de40
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-01-21 16:51:29 +0200

    limit api states by account level if not requesting your own data

--- src/main.ts
+++ src/main.ts
@@ -372,13 +372,21 @@ app.get('/api/v3/states', (req: any, res: any) => {
 app.post("/api/v3/states", (req:any, res:any) => {
   if (req.body) {
     // find state by username
+    if (req.body.username != req.user.username) {
+      if (req.user.level < 100) {
+        res.json([])
+        return;
+      }
+    }
+    
     // todo filter by permission/level
     if (req.body.username) {
       db.users.findOne({username:req.body.username}, (e:Error, user:any)=>{
         
         if (e) { res.json({error: "db error"})}
         if (user) {
-          
+            
+
           db.states.find({ apikey: user.apikey }, (er:Error, states: any[]) => {
             var cleanStates:any = []
             for (var a in states) {
