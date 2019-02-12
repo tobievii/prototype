6b11f3c7ad9a065906ad3f425fa146d0c7fcbaa9
Simphiwe <simphiwe.zulu@iotnxt.com>
2019-02-12 09:02:30 +0200

    -feat: map loads all devices and has live tracking

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -25,42 +25,40 @@ export class MapDevices extends Component {
 
   render() {
     
-    const position1 = [this.state.lat+1, this.state.lng]
-    const position2 = [this.state.lat, this.state.lng+2]
     var allDevices = this.props.devices;
     var deviceSelected = this.props.deviceCall;
     
-    allDevices.map((p,i)=>{
+    allDevices.map((p,index)=>{
       if(deviceSelected != undefined){
         if(p.devid == deviceSelected.e){
           this.setvalues(p);
         }
       }
-    })
+    });
 
     var position = [details.lat, details.lng]
 
+    allDevices.map((marker, index) => {
+      
+    });    
+    
     return (
-      <Map className="map" center={position} zoom={this.state.zoom}>
+      <Map className="map" center={position} zoom={details.zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            <h4 className="popup">Iot.Nxt</h4> <br />
-          </Popup>
-        </Marker>
-        <Marker position={position1}>
-          <Popup>
-            <h4 className="popup">Test1</h4> <br />
-          </Popup>
-        </Marker>
-        <Marker position={position2}>
-          <Popup>
-            <h4 className="popup">Test2</h4> <br />
-          </Popup>
-        </Marker>
+        {
+          allDevices.map((marker, index) => {
+            return(
+              <Marker position={[marker.payload.data.gps.lat, marker.payload.data.gps.lon]} key={marker.devid}>
+                <Popup>
+                  <h5 className="popup">{marker.devid}</h5> <br />
+                </Popup>
+              </Marker>
+            )
+          })
+        }
       </Map>
     )
   }
