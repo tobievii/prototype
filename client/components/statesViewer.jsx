da4972242b7aa1c3f534eba4cc6e81eab9d97d99
Vincent Kgadima <kgadima.khutso@gmail.com>
2019-02-27 10:22:41 +0200

    - feat: Notifications

--- client/components/statesViewer.jsx
+++ client/components/statesViewer.jsx
@@ -14,6 +14,7 @@ import { StatesViewerMenu } from "./statesViewerMenu.jsx"
 import { StatesViewerItem } from "./statesViewerItem.jsx"
 import { MapDevices } from "./map.jsx"
 
+
 library.add(faSort)
 library.add(faSortNumericDown);
 library.add(faSortAlphaDown);
@@ -167,6 +168,7 @@ export class StatesViewer extends Component {
         console.log(info);
         if (info.newdevice) {
           p.statesByUsername(this.props.username, (states) => {
+            // this.props.account.notification.type = "New device added"
             for (var s in states) {
               states[s].selected = false
             }
