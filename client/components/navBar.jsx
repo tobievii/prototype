15545c3ad9f6c7af8af28f428bab5bc7258bb492
Vincent Kgadima <46519338+VincentKgadima@users.noreply.github.com>
2019-03-20 14:07:28 +0200

    alarm notification click through

--- client/components/navBar.jsx
+++ client/components/navBar.jsx
@@ -81,13 +81,15 @@ export class Notification extends Component {
     if (this.props.notification.type == "ALARM") {
       return (
 
-        <div className="alarmNotificationItem">
-          <i className="fas fa-bullhorn"></i>
-          <span className="newdevice" >{this.newDevice()}</span><br />
-          <span className="devicename">{this.device()}</span><br />
-          <span>{this.message()}</span><br />
-          <span className="lastseen">{moment(this.props.notification.created).fromNow()}</span>
-        </div>
+        <Link to={"/u/" + this.props.account.username + "/view/" + this.device()} title="View Device Data">
+          <div className="alarmNotificationItem">
+            <i className="fas fa-bullhorn"></i>
+            <span className="newdevice" >{this.newDevice()}</span><br />
+            <span className="devicename">{this.device()}</span><br />
+            <span>{this.message()}</span><br />
+            <span className="lastseen">{moment(this.props.notification.created).fromNow()}</span>
+          </div>
+        </Link>
 
       )
     }
