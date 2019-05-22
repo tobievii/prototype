089e57b2f8b97b8b22f05a8d89114d42ca955d28
Mash <40543693+mashaole@users.noreply.github.com>
2019-05-22 15:46:36 +0200

    parsing data from device on coap server

--- src/plugins/efento/MeasurementManager.ts
+++ src/plugins/efento/MeasurementManager.ts
@@ -1,31 +1,37 @@
-const StringUtils = require('./utils/StringUtils.js');
-const DateUtils = require('./utils/DateUtils.js');
-const Measurements = require('../nbiot_pb.js');
-module.exports = class MeasurementManager {
-    static logToConsoleDataOfSensor(message) {
+import StringUtils from './StringUtils';
+import DateUtils from './DateUtils';
+import Measurements from './nbiot_pb.js';
+
+export default class MeasurementManager {
+    static logToConsoleDataOfSensor(message: any) {
         let slotsList = message.getSlotsList();
         let serialNumber = StringUtils.toHexString(message.getSerialNum());
         let isBatteryOk = message.getBatteryStatus();
         let nextTransmissionDate = DateUtils.toDateWithTimeInString(message.getNextMeasure() * 1000);
         let measurementPeriodInSec = message.getMeasurementPeriod();
         let isSensorBinary = this.isSensorBinary(slotsList);
+        let CloudToken = message.getCloudToken();
+
 
         console.log("Sensor serial number: " + serialNumber);
         console.log("Is battery ok?: " + isBatteryOk);
         console.log("Next transmission: " + nextTransmissionDate);
-        console.log("Measurement period: " + measurementPeriodInSec + " s");
-        console.log("Is sensor binary? " + isSensorBinary);
-        slotsList.map(slot => {
+        console.log("Measurement period: " + measurementPeriodInSec / 60 + " m");
+        console.log("Cloud Token: " + CloudToken);
+        // console.log("Is sensor binary? " + isSensorBinary);
+
+        slotsList.map((slot: any) => {
             this.logMeasurements(slot, isSensorBinary, measurementPeriodInSec)
         });
+
     }
 
-    static logMeasurements(slot, isSensorBinary, measurementPeriod) {
+    static logMeasurements(slot: any, isSensorBinary: any, measurementPeriod: any) {
         let measurementTimestampInSec = slot.getTimestamp();
         let startingPoint = slot.getStartPoint();
         let measurementType = slot.getType();
         let offsets = slot.getSampleOffsetsList();
-        offsets.map(offset => {
+        offsets.map((offset: any) => {
             if (offset !== -32768 && slot !== proto.Slot.SlotType.RELAY && slot !== proto.Slot.SlotType.NO_SENSOR) {
                 let measurementDate = DateUtils.toDateWithTimeInString(measurementTimestampInSec * 1000);
                 let value = this.mapMeasurementValue(measurementType, startingPoint, offset);
@@ -37,7 +43,7 @@ module.exports = class MeasurementManager {
         });
     }
 
-    static mapMeasurementValue(measurementType, startingPoint, offset) {
+    static mapMeasurementValue(measurementType: any, startingPoint: any, offset: any) {
         switch (measurementType) {
             case proto.Slot.SlotType.TEMPERATURE:
                 return "" + (startingPoint + offset) / 10.0 + " C";
@@ -52,8 +58,8 @@ module.exports = class MeasurementManager {
         }
     }
 
-    static isSensorBinary(slotsList) {
-        slotsList.map(slot => {
+    static isSensorBinary(slotsList: any) {
+        slotsList.map((slot: any) => {
             if (slot.getType() === proto.Slot.SlotType.RELAY) {
                 return true
             }
@@ -62,4 +68,3 @@ module.exports = class MeasurementManager {
         return false;
     }
 };
-export default MeasurementManager;
--- src/plugins/efento/plugin_efento.ts
+++ src/plugins/efento/plugin_efento.ts
@@ -1,19 +1,18 @@
-//import { MeasurementManager } from './utils/MeasurementManager';
-const fs = require('fs');
+import MeasurementManager from "./MeasurementManager";
+var Measurements = require('./nbiot_pb');
 var coap = require('coap');
-var server = coap.createServer();
+export const server = coap.createServer();
 export const name = "efento";
+
 server.on('request', function (req: any, res: any) {
     let requestUrl = req.url.split('/')[1];
     if (requestUrl === 'm') {
         let payload = req.payload;
-        //let message = proto.Measurements.deserializeBinary(payload);
-        //MeasurementManager.logToConsoleDataOfSensor(message);
+        var message = proto.Measurements.deserializeBinary(payload);
+        MeasurementManager.logToConsoleDataOfSensor(message);
         res.statusCode = '2.01';
         res.end(''); // put here optional payload
-        //console.log(res.payload)
     }
 });
 
 server.listen();
-
