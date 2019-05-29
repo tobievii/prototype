adb62e9482250f34ea458169d651a1a5750169ea
Mash <40543693+mashaole@users.noreply.github.com>
2019-05-29 16:12:44 +0200

    scrollable log

--- client/components/dashboard/device_history.jsx
+++ client/components/dashboard/device_history.jsx
@@ -1,10 +1,67 @@
 import React, { Component } from "react";
 import moment from 'moment'
-
+import socketio from "socket.io-client";
 export class DeviceHistory extends React.Component {
     state = {
         logdata: [],
     }
+    // socket = undefined;
+
+    // constructor(props) {
+    //     super(props)
+    //     this.socket = socketio({ transports: ['websocket', 'polling'] });
+    //     this.socket.on("connect", a => {
+    //         this.socket.emit("join", this.props.username)
+    //         fetch("/api/v3/packets", {
+    //             method: "POST", headers: { "Accept": "application/json", "Content-Type": "application/json" },
+    //             body: JSON.stringify({ log: true })
+    //         }).then(response => response.json()).then(logdata => {
+    //             this.setState({ logdata })
+    //         }).catch(err => console.error(err.toString()));
+    //         this.socket.on("post", (logdata) => {
+    //             this.handleDevicePacket(logdata)
+    //         })
+    //     })
+    // }
+
+    // handleDevicePacket = (packet) => {
+    //     var devices = _.clone(this.state.devicesServer)
+    //     var found = 0;
+    //     for (var dev in devices) {
+    //         if (devices[dev].devid == packet.id) {
+    //             found = 1;
+    //             devices[dev]["_last_seen"] = packet.timestamp;
+    //             devices[dev].payload = _.merge(devices[dev].payload, packet)
+    //         } else if (devices[dev].devid == packet.devid) {
+    //             if (packet.boundaryLayer != undefined) {
+    //                 found = 1;
+    //                 devices[dev]["_last_seen"] = packet._last_seen;
+    //                 devices[dev].payload.timestamp = packet._last_seen;
+    //                 devices[dev].boundaryLayer = packet.boundaryLayer;
+    //             } else {
+    //                 found = 1;
+    //                 devices[dev]["_last_seen"] = packet._last_seen;
+    //                 devices[dev].payload.timestamp = packet._last_seen;
+    //                 packet.selectedIcon = true;
+    //                 devices[dev] = _.merge(devices[dev].boundaryLayer, packet);
+    //             }
+    //         }
+    //     }
+
+    //     if (found == 0) {
+    //         // console.log("recieved data for device not on our list yet.")
+    //     } else {
+    //         // update
+    //         if (this.state.search.length > 0) {
+    //             this.setState({ devicesServer: devices })
+    //         } else {
+    //             this.setState({ devicesServer: devices })
+    //             this.setState({ devicesView: devices }, () => {
+    //             })
+    //             //this.sort();
+    //         }
+    //     }
+    // }
 
 
     componentDidMount() {
@@ -29,6 +86,7 @@ export class DeviceHistory extends React.Component {
                     label={"Logs"}
                     options={this.options}
                     dash={this.props.dash}
+                    style={{ height: "100%", overflowY: "scroll" }}
                 >
                     {this.state.logdata.map(function (logdata, i) {
                         return ([
@@ -47,7 +105,8 @@ export class DeviceHistory extends React.Component {
                                 </div>
                             </div>
                         ]);
-                    })}
+                    })
+                    }
                 </div>
             )
         } else {
