381cf0719f698ce64e1c67ac72bea20e058e8c19
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-06-02 13:00:20 +0200

    TCP plugin converted to new format

--- client/plugins/config.ts
+++ client/plugins/config.ts
@@ -4,12 +4,13 @@ plugins.push(require("./admin/admin_main.jsx"))
 plugins.push(require("./account/pluginAccount.jsx"))
 plugins.push(require("./iotnxt/pluginIotnxt.jsx"))
 plugins.push(require("./http/pluginHTTP.jsx"))
+plugins.push(require("./tcp/pluginTcp.jsx"))
 
 // todo: convert to cluster compatible plugins:
 
 // plugins.push(require("./discord/discord.jsx"))
 
-// plugins.push(require("./tcp/pluginTcp.jsx"))
+
 // plugins.push(require("./teltonika/plugin_teltonika.jsx"))
 // plugins.push(require("./hf2111a/client_hf2111a.jsx"))
 // plugins.push(require("./Efento/plugin_efento.jsx"))
--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -5,6 +5,7 @@ import { PluginAdmin } from "./admin/admin"
 import { PluginAccount } from "./account/account"
 import { PluginIotnxt } from "./iotnxt/plugin_iotnxt"
 import { PluginHTTP } from "./http/pluginHTTP_serverside"
+import { PluginTCP } from "./tcp/pluginTcp_serverside"
 
 export function pluginsInitialize(config: any, app: express.Express, db: any, eventHub: events.EventEmitter) {
   var plugins: any = [];
@@ -17,6 +18,7 @@ export function pluginsInitialize(config: any, app: express.Express, db: any, ev
   plugins.push(new PluginAccount(config, app, db, eventHub));
   plugins.push(new PluginIotnxt(config, app, db, eventHub));
   plugins.push(new PluginHTTP(config, app, db, eventHub));
+  plugins.push(new PluginTCP(config, app, db, eventHub));
 
   return plugins;
 }
@@ -27,7 +29,7 @@ import * as account from "./account/account"
 import * as admin from "./admin/admin"
 // import * as serialports from "./serialports/serialports"
 import * as efento from "./efento/plugin_efento"
-import * as tcpPlugin from "./tcp/pluginTcp_serverside"
+
 import * as discord from "./discord/discord"
 import * as mqttPlugin from "./mqttserver/mqttPlugin"
 
