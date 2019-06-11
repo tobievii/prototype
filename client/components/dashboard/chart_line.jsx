4c7ea432978e88216feca27b4d15a1fb9ac49a97
Daniel Sikor <daniel.sikor@iotnxt.com>
2019-06-11 16:17:59 +0200

    Fixed Chartline widget - Just needs some tweaking

--- client/components/dashboard/chart_line.jsx
+++ client/components/dashboard/chart_line.jsx
@@ -1,13 +1,10 @@
 // import React, { Component } from "react";
-import { Line } from 'react-chartjs-2';
-import { Widget } from "./widget.jsx"
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import { Charts, ChartContainer, ChartRow, YAxis, LineChart } from "react-timeseries-charts"
-import { TimeSeries, TimeRange } from "pondjs";
+import { Widget } from "./widget.jsx"
+
 import { AmchartsReact } from 'amchart4-react'
 
 am4core.useTheme(am4themes_animated);
@@ -18,60 +15,105 @@ export class ChartLine extends React.Component {
     dateAxis: null
   }
 
-  componentDidMount() {
-    if (this.props.state) {
-      if (this.props.state.key) {
-        this.createData(this.props.state.key, this.props.datapath)
-        const chart = am4core.create("chartLine", am4charts.XYChart);
-        console.log(chart);
-        this.createChart(chart);
+  constructor(props) {
+    super(props)
 
-        this.setState(() => ({ chart }))
-      }
+    console.log(props)
+  }
+
+  componentDidMount() {
+    this.createData();
+  }
+
+  createLine = (chart) => {
+    const middleLine = chart.plotContainer.createChild(am4charts.AxisLine);
+    middleLine.strokeOpacity = 1;
+    middleLine.stroke = am4core.color("#000000");
+    middleLine.strokeDasharray = "";
+    middleLine.zIndex = 1;
+    return middleLine;
+  }
+
+  // componentDidMount() {
+  //   if (this.props.state) {
+  //     if (this.props.state.key) {
+  //       this.createData(this.props.state.key, this.props.datapath)
+  //       const chart = am4core.create("chartLine", am4charts.XYChart);
+  //       console.log(chart);
+  //       this.createChart(chart);
+
+  //       this.setState(() => ({ chart }))
+  //     }
+  //   }
+  // }
+  // createData(key, datapath) {
+  //   fetch("/api/v3/packets", {
+  //     method: "POST", headers: { "Accept": "application/json", "Content-Type": "application/json" },
+  //     body: JSON.stringify({ key, datapath })
+  //   }).then(response => response.json()).then(result => {
+  //     //console.log(result);
+
+  //     var linedata = [{
+  //       id: datapath,
+  //       color: "rgb(0, 255, 255)",
+  //       data: result
+  //     }]
+
+  //     this.setState({ linedata })
+  //   }).catch(err => console.error(err.toString()));
+  // }
+
+  componentWillUnmount() {
+    if (this.state.chart) {
+      this.state.chart.dispose();
     }
   }
-  createData(key, datapath) {
+
+  createData = () => {
+    const data1 = [];
+    let price1 = 1000;
+    for (let i = 0; i < 10; i++) {
+      price1 += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 100);
+      data1.push({ date1: new Date(2015, 0, i), price1 });
+    }
+    console.log(data1)
+    var data = [];
     fetch("/api/v3/packets", {
       method: "POST", headers: { "Accept": "application/json", "Content-Type": "application/json" },
-      body: JSON.stringify({ key, datapath })
+      body: JSON.stringify(
+        {
+          key: this.props.state.key,
+          datapath: this.props.datapath
+        }
+      )
     }).then(response => response.json()).then(result => {
-      //console.log(result);
-
-      var linedata = [{
-        id: datapath,
-        color: "rgb(0, 255, 255)",
-        data: result
-      }]
-
-      this.setState({ linedata })
+      var test = []
+      for (var date in result) {
+        var f = {
+          date1: new Date(parseInt(result[date].x), 0, date),
+          price1: result[date].y
+        }
+        test.push(f)
+      }
+      console.log(test)
+      data = result;
+      this.createChart(test)
     }).catch(err => console.error(err.toString()));
   }
 
-  componentWillUnmount() {
-    if (this.chart) {
-      this.chart.dispose();
-    }
-  }
-
-
-  //renderLine() {
-  // var labels = [];
-  // var data = [];
-
-  //for (var chart of this.state.linedata[0].data) {
-  createChart = (chart) => {
-    console.log(chart.linedata)
-    chart.linedata = this.props.linedata;
+  createChart = (result) => {
+    const chart = am4core.create("chartLine", am4charts.XYChart);
+    chart.data = result;
 
     const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis.renderer.grid.template.location = 0;
-    dateAxis.renderer.labels.template.fill = am4core.color("#ffffff");
+    dateAxis.renderer.labels.template.fill = am4core.color("#e59165");
 
     this.setState(() => ({ dateAxis }))
 
     const dateAxis2 = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis2.renderer.grid.template.location = 0;
-    dateAxis2.renderer.labels.template.fill = am4core.color("#ffffff");
+    dateAxis2.renderer.labels.template.fill = am4core.color("#dfcc64");
 
     const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
     valueAxis.tooltip.disabled = true;
@@ -86,15 +128,15 @@ export class ChartLine extends React.Component {
 
     const axisRange = dateAxis.axisRanges.create();
     axisRange.date = new Date(2015, 0, 5);
+    // axisRange.value = 1200;
     axisRange.grid.stroke = am4core.color("#A96478");
     axisRange.grid.strokeWidth = 2;
     axisRange.grid.strokeOpacity = 1;
+    // axisRange.label.text = "middle";
     axisRange.label.fill = axisRange.grid.stroke;
     axisRange.label.verticalCenter = "bottom";
-    console.log('axis', axisRange);
 
     const series = chart.series.push(new am4charts.LineSeries());
-    series.name = "2016";
     series.dataFields.dateX = "date1";
     series.dataFields.valueY = "price1";
     series.tooltipText = "[bold]{valueY}[/]";
@@ -107,9 +149,12 @@ export class ChartLine extends React.Component {
     axisTooltip.background.cornerRadius = 3;
     axisTooltip.background.pointerLength = 0;
     axisTooltip.dy = 5;
+    //series.strokeWidth = 3;
+    // series.columns.template.events.on("hit", function (ev) {
+    //   console.log("clicked on ", ev.target);
+    // }, this);
 
     const series2 = chart.series.push(new am4charts.LineSeries());
-    series2.name = "2017";
     series2.dataFields.dateX = "date2";
     series2.dataFields.valueY = "price2";
     series2.yAxis = valueAxis2;
@@ -125,11 +170,16 @@ export class ChartLine extends React.Component {
     axisTooltip2.background.pointerLength = 0;
     axisTooltip2.dy = 5;
 
+    // const line = this.createLine(chart);
+    // console.log(line);
+
     chart.cursor = new am4charts.XYCursor();
     chart.cursor.lineX.stroke = am4core.color("#000000");
     chart.cursor.lineX.strokeWidth = 2;
     chart.cursor.lineX.strokeDasharray = "";
     chart.cursor.lineY.strokeOpacity = 0;
+
+    // chart.cursor.behavior = "selectX";
 
     const scrollbarX = new am4charts.XYChartScrollbar();
     scrollbarX.series.push(series);
@@ -146,37 +196,24 @@ export class ChartLine extends React.Component {
     dateAxis2.renderer.grid.template.strokeOpacity = 0.07;
     dateAxis.renderer.grid.template.strokeOpacity = 0.07;
     valueAxis.renderer.grid.template.strokeOpacity = 0.07;
+    this.setState(() => ({ chart }))
 
-
-
-    return (
-      <ChartLine linedata={this.createData()} />
-    )
   }
-  // }
-  //}
 
   render() {
-
-    // if (this.state.linedata) {
-    //   return (<Widget label={this.props.data.dataname} options={this.options} dash={this.props.dash}>{this.renderLine()}</Widget>)
-    // } else {
-    //   return (<Widget label={this.props.data.dataname} options={this.options} dash={this.props.dash}></Widget>)
-    // }
-
     return (
       <div>
         <div id="chartLine" />
-        {/* {this.createChart()} */}
         {this.state.chart ?
-          <AmchartsReact
-            chart={this.state.chart}
-            xAxis={this.state.dateAxis}
-            color={am4core.color("#838383")}
-          />
-
-          : null}
-        {/* <ChartLine linedata={this.createData} /> */}
+          <Widget label={this.props.data.dataname} dash={this.props.dash}>
+            <AmchartsReact
+              chart={this.state.chart}
+              xAxis={this.state.dateAxis}
+              color={am4core.color("#838383")}
+            />
+          </Widget>
+          : <Widget label={this.props.data.dataname} dash={this.props.dash}></Widget>}
+      //: null}
       </div>
     );
   }
