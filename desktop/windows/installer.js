78135f9dbefcecc501d40a684415fbd541b114c0
Vincent Kgadima <46519338+VincentKgadima@users.noreply.github.com>
2019-06-14 08:35:36 +0200

    Compiler update

--- desktop/windows/installer.js
+++ desktop/windows/installer.js
@@ -2,13 +2,9 @@ var electronInstaller = require('electron-winstaller');
 
 // In this case, we can use relative paths
 var settings = {
-    // Specify the folder where the built app is located
     appDirectory: './PROTOTYP3-win32-x64',
-    // Specify the existing folder where 
     outputDirectory: './PROTOTYPE-installer',
-    // The name of the Author of the app (the name of your company)
     authors: 'IoT.nxt.',
-    // The name of the executable of your built
     exe: './PROTOTYP3.exe'
 };
 
