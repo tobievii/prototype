eafea28390306a20138b51628d732ab652b729ac
Vincent Kgadima <46519338+VincentKgadima@users.noreply.github.com>
2019-06-14 08:27:27 +0200

    Log adjustments

--- desktop/windows/installer.js
+++ desktop/windows/installer.js
@@ -15,7 +15,7 @@ var settings = {
 resultPromise = electronInstaller.createWindowsInstaller(settings);
 
 resultPromise.then(() => {
-    console.log("The installers of your application were succesfully created !");
+    console.log("The installers of your application were succesfully created!");
 }, (e) => {
-    console.log(`Well, sometimes you are not so lucky: ${e.message}`)
+    console.log(`Error: Could not create installer ${e.message}`)
 });
