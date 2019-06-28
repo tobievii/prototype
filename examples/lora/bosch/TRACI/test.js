dab3f4a1366391124fd5000be3eb21ce72fb55d7
Vincent Kgadima <46519338+VincentKgadima@users.noreply.github.com>
2019-06-28 11:46:59 +0200

    test files

--- examples/lora/bosch/TRACI/test.js
+++ examples/lora/bosch/TRACI/test.js
@@ -34,31 +34,56 @@ var Int32 = function (value) {
     return (ref > 0x7FFFFFFF) ? ref - 0x100000000 : ref;
 };
 
-function Decoder(bytes, port) {
-    // Decode an uplink message from a buffer
-    // (array) of bytes to an object of fields.
-    var decoded = {};
+function Decode(bytes, port) {
+    var weekNumberTimeOfWeek = (bytes[16] << 24) | (bytes[15] << 16) | (bytes[14] << 8) | (bytes[13] << 0);
+    var GPSDate = new Date(1980, 0, 1, 0, 0, 0, 0);
+    var GPSWeekRollovers = 2;
+    var BluetoothBeacons = {};
+
+    var decoded = {
+        MessageType: any,
+        ProtocolVersion: any,
+        BattState: number,
+        AccVector: number,
+        AccState: number,
+        GPSLongitude: number,
+        GPSLatitude: number,
+        GPSAltitude: number,
+        GPSWeekNumber: number,
+        GPSTimeOfWeek: number,
+        GPSTimestamp: GPSDate.toString(),
+        GPSHorizontalAccuracy: number,
+        GPSVerticalAccuracy: number,
+        AccOperatingMinutes1: number,
+        AccOperatingMinutes2: number,
+        AccNumberShocks: any,
+        Temperature: number,
+        MagneticFieldX: number,
+        MagneticFieldY: number,
+        MagneticFieldZ: number,
+        BTNum: any,
+        SoftwareVersion: any,
+        BootblockVersion: any,
+        HardwareVersion: any,
+        RegionCode: any,
+        ConfigurationMessageId: any,
+        ConfigurationState: any
+    };
 
     if (port === 1) {
         decoded.ProtocolVersion = bytes[0];
         decoded.MessageType = bytes[1];
         if (decoded.MessageType === 0x00) {
-            // Standard Message Type
             decoded.BattState = (bytes[2] >> 4) & 0x07;
             decoded.AccVector = (bytes[2] >> 1) & 0x07;
             decoded.AccState = (bytes[2]) & 0x01;
             decoded.GPSLongitude = Int32((bytes[6] << 24) | (bytes[5] << 16) | (bytes[4] << 8) | (bytes[3] << 0)) / 10000000;
             decoded.GPSLatitude = Int32((bytes[10] << 24) | (bytes[9] << 16) | (bytes[8] << 8) | (bytes[7] << 0)) / 10000000;
             decoded.GPSAltitude = Int16(bytes[12] << 8) | (bytes[11] << 0);
-            weekNumberTimeOfWeek = (bytes[16] << 24) | (bytes[15] << 16) | (bytes[14] << 8) | (bytes[13] << 0);
             decoded.GPSWeekNumber = (weekNumberTimeOfWeek >> 20) & 0x3FF;
             decoded.GPSTimeOfWeek = weekNumberTimeOfWeek & 0x000FFFFF;
-
-            var GPSDate = new Date(1980, 0, 1, 0, 0, 0, 0); // reference date 1. january 1980
-            var GPSWeekRollovers = 2;
-            // add weeknumber in days relative to 1st january 1980; add 6 because weeks started at 6th january 1980
             GPSDate.setDate(6 + ((decoded.GPSWeekNumber + (GPSWeekRollovers * 1024)) * 7));
-            GPSDate.setSeconds(decoded.GPSTimeOfWeek); // add seconds to gps week
+            GPSDate.setSeconds(decoded.GPSTimeOfWeek);
             decoded.GPSTimestamp = GPSDate.toString();
             decoded.GPSHorizontalAccuracy = ((bytes[18] << 8) | (bytes[17] << 0)) / 10;
             decoded.GPSVerticalAccuracy = ((bytes[20] << 8) | (bytes[19] << 0)) / 10;
@@ -73,13 +98,11 @@ function Decoder(bytes, port) {
         }
 
         if (decoded.MessageType === 0x01) {
-            // Alive message type
             decoded.Temperature = Int8(bytes[2]) + 23;
             decoded.BTNum = bytes[3];
         }
 
         if (decoded.MessageType === 0x02) {
-            // startup message type
             decoded.SoftwareVersion = bytes.slice(2, 17);
             decoded.BootblockVersion = bytes.slice(18, 33);
             decoded.HardwareVersion = bytes.slice(34, 49);
@@ -87,14 +110,11 @@ function Decoder(bytes, port) {
         }
 
         if (decoded.MessageType === 0x5F) {
-            // configurtaion acknowledged message type
             decoded.ConfigurationMessageId = bytes[2];
             decoded.ConfigurationState = bytes[3];
         }
 
         if (decoded.MessageType === 0x10) {
-            // bluetooth collection message type
-            var BluetoothBeacons = {};
             for (var cnt = 0; cnt < 4; cnt++) {
                 MACAdress = bytes.slice((cnt * 7) + 2, (cnt * 7) + 8).map(function (byte) {
                     return ('0' + (byte & 0xFF).toString(16)).slice(-2);
@@ -110,5 +130,6 @@ function Decoder(bytes, port) {
         "data": decoded
     };
 
-    return Tracker;
+    return Tracker
+
 }
