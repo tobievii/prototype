ed4167dde8856f64591ab818f8cdb9f53f647bba
Vincent Kgadima <46519338+VincentKgadima@users.noreply.github.com>
2019-07-22 17:44:53 +0200

    API getdevice

--- native/src/containers/menu/DeviceList/device.list.container.tsx
+++ native/src/containers/menu/DeviceList/device.list.container.tsx
@@ -1,37 +1,60 @@
-import React from "react";
+// TODO Display the data
+import React from 'react';
 import { NavigationScreenProps } from "react-navigation";
-import { Layouts } from "./device.list.component";
-import { DeviceListContainerData } from "./type";
-import { routes } from "./routes";
+import { any } from 'prop-types';
+import { Layouts } from './device.list.component';
+import { Text } from 'react-native';
+// import { DeviceListContainerData } from "./type";
+// import { routes } from "./routes";
 
 export class DeviceListContainer extends React.Component<NavigationScreenProps> {
 
-  componentDidMount = () => {
-    fetch("https://prototype.dev.iotnxt.io/api/v3/states", {
-      method: "GET",
-      headers: {
-        "Authorization": "Basic YXBpOmtleS1iZDBkdXJucDlncDZyenp0azFsNjh5dzl3NHVodXM2OA==",
-        "Content-Type": "application/json"
-      },
-    })
-      .then(response => response.json())
-      .catch(err => console.error(err.toString()));
-
-  }
-
-  private data: DeviceListContainerData[] = routes;
-  private navigationKey: string = "DeviceListContainer";
-
-  private onItemSelect = (index: number) => {
-    const { [index]: selectedItem } = this.data;
-
-    this.props.navigation.navigate({
-      key: this.navigationKey,
-      routeName: selectedItem.route,
-    });
+  state = {
+    data: any,
   };
 
-  public render(): React.ReactNode {
-    return <Layouts data={this.data} onItemSelect={this.onItemSelect} />;
+  getDeviceList = async () => {
+    // var data = await response.json()
+    try {
+      const response = await fetch("https://prototype.dev.iotnxt.io/api/v3/states", {
+        method: "GET",
+        headers: {
+          "Authorization": "Basic YXBpOmtleS1iZDBkdXJucDlncDZyenp0azFsNjh5dzl3NHVodXM2OA==",
+          "Content-Type": "application/json"
+        },
+      });
+      const data = await response.json();
+
+      this.setState({ data: data });
+      // const data = responseJson;
+      // console.log(data)
+    }
+    catch (err) {
+      return console.error(err.toString());
+    }
+    // return responseJson;
   }
+
+  componentDidMount = () => {
+    this.getDeviceList();
+  }
+
+  // private data: DeviceListContainerData[] = routes;
+  private navigationKey: string = "DeviceListContainer";
+
+  // private onItemSelect = (index: number) => {
+  //   const { [index]: selectedItem } = this.data;
+
+  //   this.props.navigation.navigate({
+  //     key: this.navigationKey,
+  //     routeName: selectedItem.route,
+  //   });
+  // };
+
+  // public render(): React.ReactNode {
+  //   return <Text>{this.state.data}</Text>;
+  // }
+
+  render = () => <Text>HEllo</Text>;
+  // render = () => <Text>{this.state.data}</Text>;
 }
