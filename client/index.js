d892bd8fa3c768d8d3bcd76464ab6512d44924ed
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-08-16 10:55:16 +0200

    clear branch

--- client/index.js
+++
@@ -1,9 +0,0 @@
-import React, {
-    Component
-} from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-
-
-const root = document.querySelector('#app')
-ReactDOM.render( < App / > , root);
--- src/tests/dev.ts
+++
@@ -1,5 +0,0 @@
-import { TeltonikaDeviceSim } from "../plugins/teltonika/test_teltonika"
-
-var device = new TeltonikaDeviceSim({host:"localhost", port: 12002 })
-
-device.test()
