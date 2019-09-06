c7d8e53abb50ffd0628a857996cb523b554348a6
Mash <40543693+mashaole@users.noreply.github.com>
2019-09-06 11:28:04 +0200

    device view drop down for data

--- native/src/containers/menu/DeviceView/dataView.tsx
+++ native/src/containers/menu/DeviceView/dataView.tsx
@@ -1,33 +1,56 @@
 import React, { Component } from 'react';
 import { NavigationScreenProps } from 'react-navigation';
 import { Text, View, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export class DataView extends React.Component {
     state: {
         open;
+        arrow;
+        preview;
     }
 
     componentWillMount() {
         this.setState({ open: "none" })
+        this.setState({ preview: "" })
+        this.setState({ arrow: "caretright" })
     }
     open = () => {
         if (this.state.open == "none") {
             this.setState({ open: "" })
+            this.setState({ preview: "none" })
+            this.setState({ arrow: "caretdown" })
+        }
+        else {
+            this.setState({ open: "none" })
+            this.setState({ preview: "" })
+            this.setState({ arrow: "caretright" })
+        }
+    }
+
+    preview = () => {
+        if (this.state.preview == "") {
+            return (<Text style={{ display: "none", fontSize: 12, width: 10, opacity: 0.3 }}>
+                {JSON.stringify(this.props['data'].data).substr(0, 22) + ".....(Click here to expand)"}
+            </Text>)
+        }
+        else {
+            return null
         }
     }
     renderData = (data, level, path) => {
         if (data == null) { }
         if (typeof data == "string") {
-            return <View ><Text style={{ textAlign: "right", color: "#ccc", marginRight: 5 }}>{data}</Text></View>
+            return <View ><Text style={{ color: "#ccc", textAlign: "right", marginRight: 5 }}>{data}</Text></View>
         }
         if (typeof data == "number") {
-            return <View><Text style={{ textAlign: "right", color: "#15e47a", marginRight: 5 }}>{data}</Text></View>
+            return <View><Text style={{ color: "#15e47a", textAlign: "right", marginRight: 5 }}>{data}</Text></View>
         }
         if (typeof data == "boolean") {
             if (data == true) {
-                return <View style={{ alignItems: 'flex-end' }}><Text style={{ textAlign: "right", color: "#e4c315", marginRight: 5 }}>{data.toString()}</Text></View>
+                return <View style={{}}><Text style={{ color: "#e4c315", textAlign: "right", marginRight: 5 }}>{data.toString()}</Text></View>
             } else {
-                return <View><Text style={{ textAlign: "right", color: "#15b9e4", marginRight: 5 }}>{data.toString()}</Text></View>
+                return <View><Text style={{ color: "#15b9e4", textAlign: "right", marginRight: 5 }}>{data.toString()}</Text></View>
             }
 
         }
@@ -51,19 +74,32 @@ export class DataView extends React.Component {
 
     renderObject = (data, level, path) => {
         return (
-            <View style={{ backgroundColor: "rgba(212, 211, 211,0.1)" }}>
+            <View style={{ backgroundColor: "rgba(212, 211, 211,0.09)" }}>
                 {Object.keys(data).map((name, i) => {
 
                     if (typeof data[name] == "object") {
-                        return (
-                            <View style={{ marginLeft: 24, backgroundColor: "rgba(212, 211, 211,0.3)" }} key={i}>
-                                <Text style={{ color: "white" }}>{name}:</Text>
-                                <View style={{ display: "" }}>{this.renderData(data[name], level, path + "." + name)}</View>
-                            </View>)
+                        if (name == "data") {
+                            return (
+                                <TouchableHighlight onPress={() => { this.open() }} key={i}>
+                                    <View style={{ marginLeft: 24, marginBottom: 10 }} >
+                                        <View>
+                                            <Text style={{ color: "white", fontSize: 20, flexDirection: "row" }}><AntDesign name={this.state.arrow} size={20} color="white" />  {name}:{this.preview()}</Text></View>
+                                        <View style={{ display: this.state.open }}>{this.renderData(data[name], level, path + "." + name)}</View>
+                                    </View></TouchableHighlight>)
+                        }
+                        else {
+                            return (
+                                <TouchableHighlight onPress={() => { this.open() }} key={i}>
+                                    <View style={{ marginLeft: 24, marginBottom: 10 }} >
+                                        <View>
+                                            <Text style={{ color: "white", fontSize: 20, flexDirection: "row" }}><AntDesign name={this.state.arrow} size={20} color="white" />  {name}:</Text></View>
+                                        <View style={{ display: this.state.open }}>{this.renderData(data[name], level, path + "." + name)}</View>
+                                    </View></TouchableHighlight>)
+                        }
                     } else {
                         return (
                             <View style={{ marginLeft: 12 }} key={i}  >
-                                <Text style={{ color: "white", flexDirection: 'row' }}>{name}:</Text>
+                                <Text style={{ color: "white" }}>{name}:</Text>
                                 <View >{this.renderData(data[name], level, path + "." + name)}</View>
                             </View>)
                     }
@@ -74,17 +110,17 @@ export class DataView extends React.Component {
 
     render() {
 
-        if (this.props.data) {
-            if (this.props.data.payload) {
+        if (this.props['data']) {
+            if (this.props['data'].payload) {
                 return (
                     <View>
-                        {this.renderObject(this.props.data.payload, 0, "root")}
+                        {this.renderObject(this.props['data'].payload, 0, "root")}
                     </View>
                 );
             } else {
                 return (
                     <View>
-                        {this.renderObject(this.props.data, 0, "root")}
+                        {this.renderObject(this.props['data'], 0, "root")}
                     </View>
                 );
             }
