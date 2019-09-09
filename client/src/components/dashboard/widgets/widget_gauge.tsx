6f8bc6ae73a78c4992ce16b39f4cba620ae3e35a
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-09-09 12:51:05 +0200

    widget guage undermin and overmax

--- client/src/components/dashboard/widgets/widget_gauge.tsx
+++ client/src/components/dashboard/widgets/widget_gauge.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Vector } from "../../../../../server/shared/vector"
-
 import { WidgetComponent } from "./widgetcomponent"
+import { colors } from "../../../theme"
 
 export default class WidgetGauge extends WidgetComponent {
 
@@ -51,7 +51,8 @@ export default class WidgetGauge extends WidgetComponent {
 
     drawguageSvg(min, value, max, color) {
 
-
+        if (value > max) { value = max };
+        if (value < min) { value = min }
 
         if (this.state.typeError) {
             return null;
@@ -63,7 +64,6 @@ export default class WidgetGauge extends WidgetComponent {
             var graphdegree = ((180 + 35 + 35) * ratio) - 35
             return (<path className="value" fill="none" stroke={color} strokeWidth="1" d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(graphdegree))}></path>)
         }
-
     }
 
 
@@ -86,42 +86,63 @@ export default class WidgetGauge extends WidgetComponent {
         var value = 0;
         if (this.props.value) { value = parseInt(this.props.value) }
 
+        var undermin = (value <= min) ? true : false
+        var overmax = (value >= max) ? true : false
+
         return (
             //<Widget label={this.props.data.dataname} options={this.options} dash={this.props.dash} setOptions={this.setOptions}>
-            <div style={{ width: "80%", height: "80%", margin: "0 auto" }}>
-                <svg viewBox="0 0 100 100" className="gauge">
-                    <text
-                        x="50"
-                        y="50"
-                        fill="#fff"
-                        className="value-text"
-                        fontSize="100%"
-                        fontWeight="normal"
-                        textAnchor="middle"
-                        alignmentBaseline="middle"
-                        dominantBaseline="central">{value}</text>
+            <div style={{ margin: "0 auto" }}>
 
-                    <text x="0" y="80"
-                        fill="#aaa"
-                        className="value-text"
-                        fontSize="40%"
-                        fontWeight="normal"
-                        textAnchor="start"
-                        //alignmentBaseline="top"
-                        dominantBaseline="central">MIN:{Math.round(min)}</text>
+                {(overmax)
+                    ? <div style={{ color: colors.warning, position: "absolute" }}><i
+                        className="fas fa-exclamation-triangle" /> OVER MAX!</div>
+                    : <div></div>}
 
-                    <text x="100" y="80"
-                        fill="#aaa"
-                        className="value-text"
-                        fontSize="40%"
-                        fontWeight="normal"
-                        textAnchor="end"
-                        //alignmentBaseline="top"
-                        dominantBaseline="central">MAX:{Math.round(max)}</text>
+                {(undermin)
+                    ? <div style={{ color: colors.warning, position: "absolute" }}><i
+                        className="fas fa-exclamation-triangle" /> UNDER MIN!</div>
+                    : <div></div>}
 
-                    <path className="value" fill="none" stroke="rgba(255,255,255,0.1)" strokeWidth="1" d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(180 + 35))}></path>
-                    {this.drawguageSvg(min, value, max, color)}
-                </svg>
+                <div style={{ paddingTop: 10, overflow: "hidden", width: "80%", margin: "0 auto" }}>
+                    <svg viewBox="0 0 100 90" className="gauge">
+                        <text
+                            x="50"
+                            y="50"
+                            fill="#fff"
+                            className="value-text"
+                            fontSize="100%"
+                            fontWeight="normal"
+                            textAnchor="middle"
+                            alignmentBaseline="middle"
+                            dominantBaseline="central">{value}</text>
+
+                        <text x="0" y="80"
+                            fill="#aaa"
+                            className="value-text"
+                            fontSize="40%"
+                            fontWeight="normal"
+                            textAnchor="start"
+                            //alignmentBaseline="top"
+                            dominantBaseline="central">MIN:{Math.round(min)}</text>
+
+                        <text x="100" y="80"
+                            fill="#aaa"
+                            className="value-text"
+                            fontSize="40%"
+                            fontWeight="normal"
+                            textAnchor="end"
+                            //alignmentBaseline="top"
+                            dominantBaseline="central">MAX:{Math.round(max)}</text>
+
+
+
+                        <path className="value" fill="none" stroke="rgba(255,255,255,0.1)"
+                            strokeWidth="1" d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(180 + 35))}>
+
+                        </path>
+                        {this.drawguageSvg(min, value, max, color)}
+                    </svg>
+                </div>
             </div>
         );
 
