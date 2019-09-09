cb9eae8d19ceefe1ddd1e08740effd41165d0e85
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-09-09 15:11:14 +0200

    widgetGauge 5.1

--- client/src/components/dashboard/widgets/widget_gauge.tsx
+++ client/src/components/dashboard/widgets/widget_gauge.tsx
@@ -4,24 +4,12 @@ import { WidgetComponent } from "./widgetcomponent"
 import { colors } from "../../../theme"
 
 export default class WidgetGauge extends WidgetComponent {
-
-    name = "Gauge"
-
     state = {
-        value: 50,
-        min: 0,
-        max: 100,
-        valueanim: 0,
-        typeError: false,
         options: {
             min: { type: "input", default: 0, value: -1 },
             max: { type: "input", default: 100, value: 101 },
             color: { type: "color", default: "#00ff00", value: "#0000ff" }
         }
-    }
-
-    options() {
-        return this.state.options
     }
 
     polar_to_cartesian = function (cx, cy, radius, angle) {
@@ -36,71 +24,42 @@ export default class WidgetGauge extends WidgetComponent {
         var end = new Vector({ x: -radius })
         start.rotate(new Vector({ z: 1 }), start_angle)
         end.rotate(new Vector({ z: 1 }), end_angle)
-
         var largeArcFlag = "0"
-        if ((end_angle - start_angle) > Math.PI) {
-            largeArcFlag = "1"
-        }
-
+        if ((end_angle - start_angle) > Math.PI) { largeArcFlag = "1" }
         return "M " + (start.x + center.x).toFixed(3) + " " + (start.y + center.y).toFixed(3) + " A " + radius + " " + radius + " 0 " + largeArcFlag + " 1 " + (end.x + center.x) + " " + (end.y + center.y);
     };
 
-    degrees(degrees) {
-        return ((Math.PI * 2) / 360) * degrees
-    }
+    degrees(degrees) { return ((Math.PI * 2) / 360) * degrees }
 
-    drawguageSvg(min, value, max, color) {
-
+    drawguageSvg(min, value, max) {
         if (value > max) { value = max };
         if (value < min) { value = min }
-
-        if (this.state.typeError) {
-            return null;
-        } else {
-            var range = max - min;
-            var valr = value - min;
-            var ratio = valr / range;
-
-            var graphdegree = ((180 + 35 + 35) * ratio) - 35
-            return (<path className="value" fill="none" stroke={color} strokeWidth="1" d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(graphdegree))}></path>)
-        }
+        var range = max - min;
+        var valr = value - min;
+        var ratio = valr / range;
+        var graphdegree = ((180 + 35 + 35) * ratio) - 35
+        return (<path
+            className="value"
+            fill="none"
+            stroke={this.state.options.color.value}
+            strokeWidth="1"
+            d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(graphdegree))} />)
     }
 
-
-
     render() {
-
-        var min = this.state.options.min.default
-        var max = this.state.options.max.default
-        var color = this.state.options.color.default
-
-        // widget options:
-        if (this.props.widget.options) {
-            var db = this.props.widget.options;
-            if (db.min) min = db.min
-            if (db.color) color = db.color
-            if (db.max) max = db.max
-        }
-        //
-
         var value = 0;
         if (this.props.value) { value = parseInt(this.props.value) }
-
-        var undermin = (value <= min) ? true : false
-        var overmax = (value >= max) ? true : false
+        var undermin = (value <= this.state.options.min.value) ? true : false
+        var overmax = (value >= this.state.options.max.value) ? true : false
 
         return (
-            //<Widget label={this.props.data.dataname} options={this.options} dash={this.props.dash} setOptions={this.setOptions}>
             <div style={{ margin: "0 auto" }}>
-
                 {(overmax)
-                    ? <div style={{ color: colors.warning, position: "absolute" }}><i
-                        className="fas fa-exclamation-triangle" /> OVER MAX!</div>
+                    ? <div style={{ color: colors.warning, position: "absolute" }}><i className="fas fa-exclamation-triangle" /> OVER MAX!</div>
                     : <div></div>}
 
                 {(undermin)
-                    ? <div style={{ color: colors.warning, position: "absolute" }}><i
-                        className="fas fa-exclamation-triangle" /> UNDER MIN!</div>
+                    ? <div style={{ color: colors.warning, position: "absolute" }}><i className="fas fa-exclamation-triangle" /> UNDER MIN!</div>
                     : <div></div>}
 
                 <div style={{ paddingTop: 10, overflow: "hidden", width: "80%", margin: "0 auto" }}>
@@ -123,7 +82,7 @@ export default class WidgetGauge extends WidgetComponent {
                             fontWeight="normal"
                             textAnchor="start"
                             //alignmentBaseline="top"
-                            dominantBaseline="central">MIN:{Math.round(min)}</text>
+                            dominantBaseline="central">MIN:{Math.round(this.state.options.min.value)}</text>
 
                         <text x="100" y="80"
                             fill="#aaa"
@@ -132,15 +91,13 @@ export default class WidgetGauge extends WidgetComponent {
                             fontWeight="normal"
                             textAnchor="end"
                             //alignmentBaseline="top"
-                            dominantBaseline="central">MAX:{Math.round(max)}</text>
+                            dominantBaseline="central">MAX:{Math.round(this.state.options.max.value)}</text>
 
 
 
                         <path className="value" fill="none" stroke="rgba(255,255,255,0.1)"
-                            strokeWidth="1" d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(180 + 35))}>
-
-                        </path>
-                        {this.drawguageSvg(min, value, max, color)}
+                            strokeWidth="1" d={this.svg_arc_path(50, 50, 40, this.degrees(-35), this.degrees(180 + 35))} />
+                        {this.drawguageSvg(this.state.options.min.value, value, this.state.options.max.value)}
                     </svg>
                 </div>
             </div>
