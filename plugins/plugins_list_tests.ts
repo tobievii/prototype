4474207a2656963ceeabc06c09f258ecf0c58747
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-10-11 10:33:11 +0200

    add iotnxt to list of tests

--- plugins/plugins_list_tests.ts
+++ plugins/plugins_list_tests.ts
@@ -1,5 +1,6 @@
 import { default as mqtt } from './mqtt/mqtt_test'
 import { default as teltonika } from './teltonika/teltonika_test'
+import { default as iotnxt } from './iotnxt/iotnxt_test'
 
 
-export var pluginTests: any = { mqtt, teltonika }
+export var pluginTests: any = { mqtt, teltonika, iotnxt }
