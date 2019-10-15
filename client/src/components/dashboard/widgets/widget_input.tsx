41139aa07666beeacde807d7d6e42b087bb25d57
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-10-15 16:22:21 +0200

    widget input button fix

--- client/src/components/dashboard/widgets/widget_input.tsx
+++ client/src/components/dashboard/widgets/widget_input.tsx
@@ -38,15 +38,15 @@ export default class WidgetInput extends WidgetComponent {
                     boxSizing: "border-box"
                 }}>
 
-                    <div style={{ flex: 1 }}><input
+                    <div style={{ flex: 2 }}><input
                         onChange={(e) => { this.setState({ inputval: e.target.value }) }}
                         value={this.state.inputval}
                         style={{ width: "100%", height: "100%", padding: 5 }}></input>
                     </div>
 
-                    <div style={{ flex: 0 }}>
+                    <div style={{ flex: 1 }}>
 
-                        <button style={{ border: colors.borders, height: "100%", width: "100%", padding: 6, minWidth: 10, margin: 0 }}
+                        <button style={{ border: colors.borders, height: "100%", width: "100%", padding: 6, minWidth: 30, textAlign: "center", margin: 0 }}
                             onClick={e => {
                                 var data = {}
                                 data[this.state.options.name.value] = this.state.inputval;
